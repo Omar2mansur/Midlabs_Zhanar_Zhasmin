c0b76a076dc6a9d4745a21628011f983a8582002
lgalstyan <larisagalstya@gmail.com>
2023-03-10 12:39:41 +0400

    tumolabs mini_project

---
+++ project 1 Zhanar (1).py
@@ -0,0 +1,27 @@
+print('Zhanar Sholanbayeva')
+number = input ("write number")
+time = input ("Write some period of time")
+transport = input ("What type of transport do you preferred?")
+adj1 = input ("Write some adjective")
+adj2 = input("write and adjective")
+noun1 = input ("Write a noun")
+color1 = input ("What is your favorite color")
+body1 = input("Your favorite part of the body")
+verb1 = input ("write a verb")
+number2 = input ("write number again")
+noun2 = input ("Write your favorite noun")
+noun3 = input ("write a noun again")
+body2 = input ("write some part of the your body")
+verb2 = input ("write verb again")
+noun4 = input ("write a noun again")
+adj3 = input ("write adjective again")
+silly = input ("write some silly word that you have heard today")
+noun5 = input ("write your last noun")
+
+print ("It was about" + number + time +  "ago when I arrived at the hospital in a" + transport + "." +
+       "The hospoital is" +  adj1 +  "place, there are a lot of" + adj2 + noun1 + "here." +
+       "There are nurses here who have" +  color1 +  body1  + "." +
+       "If someone wants to come into my room I told them that they have to" + verb1 + "first."  +
+       "I have decorated my room with" + number2 + noun2 + "." +  "Today I talked to a doctor and they were wearing a" + noun3 +
+       "on their"  + body2 + "." + "I heard that all doctors" + verb2 + noun4 + "every day for breakfast" + "." +
+       "The most" + adj3 + "thing about being in the hospital is the" + silly + noun5 + "!" )
